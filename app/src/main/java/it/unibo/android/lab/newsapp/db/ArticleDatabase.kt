4f0547ab58d15e2af8e525f3820d4afea8161db4
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-28 16:32:43 +0200

    Add companion object, it manages single istance of the db, "Volatile" permit to visualize changes on istances and "LOCK" sync the acces to the code of the istance

--- app/src/main/java/it/unibo/android/lab/newsapp/db/ArticleDatabase.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/db/ArticleDatabase.kt
@@ -1,6 +1,9 @@
 package it.unibo.android.lab.newsapp.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import it.unibo.android.lab.newsapp.models.Article
 
@@ -10,5 +13,26 @@ import it.unibo.android.lab.newsapp.models.Article
 )
 
 @TypeConverters(Converters::class)
-abstract class ArticleDatabase {
+abstract class ArticleDatabase: RoomDatabase() {
+
+    abstract fun getArticleDao(): ArticleDAO
+
+    companion object {
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDatabase::class.java,
+                "article_db.db" // Corrected the syntax error here
+            ).build()
+    }
 }
