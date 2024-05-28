554e8d27e15664dc409c8958f86e821b2240d9e9
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-28 16:04:17 +0200

    Fixed error

--- app/src/main/java/it/unibo/android/lab/newsapp/models/Article.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/models/Article.kt
@@ -3,13 +3,10 @@ package it.unibo.android.lab.newsapp.models
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity{
-  tableName = "articles"
-}
-
+@Entity(tableName = "articles")
 data class Article(
   @PrimaryKey(autoGenerate = true)
-  var id:Int? = null,
+  var id: Int? = null,
   val author: String,
   val content: String,
   val description: String,
