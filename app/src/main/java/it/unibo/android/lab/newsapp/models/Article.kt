2d458973c13f7537644491dadaa0c95d1228c157
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-28 12:07:30 +0200

    Add "Entity class for the database table"

--- app/src/main/java/it/unibo/android/lab/newsapp/models/Article.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/models/Article.kt
@@ -1,6 +1,11 @@
 package it.unibo.android.lab.newsapp.models
 
+@Entity{
+  tableName = "articles"
+}
 data class Article(
+  @PrimaryKey(autoGenerate = true)
+  var id: Int? = null,
   val author: String,
   val content: String,
   val description: String,
