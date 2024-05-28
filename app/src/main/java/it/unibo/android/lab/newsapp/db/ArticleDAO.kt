89e827cbd346c33064c638d3eaa6267225168d30
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-28 16:01:48 +0200

    Add Query and Delete fun

--- app/src/main/java/it/unibo/android/lab/newsapp/db/ArticleDAO.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/db/ArticleDAO.kt
@@ -1,8 +1,11 @@
 package it.unibo.android.lab.newsapp.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import it.unibo.android.lab.newsapp.models.Article
 
 @Dao
@@ -10,4 +13,10 @@ interface ArticleDAO {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(article: Article): Long
+
+    @Query("SELECT *FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
 }
