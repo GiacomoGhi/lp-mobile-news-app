888d6ea81e93dcaae18fded2b502b3194333bdab
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-05-29 11:07:57 +0200

    Add the functions that controls if the items and the contents are the same

--- app/src/main/java/it/unibo/android/lab/newsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/adapters/NewsAdapter.kt
@@ -1,10 +1,15 @@
 package it.unibo.android.lab.newsapp.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import it.unibo.android.lab.newsapp.R
+import it.unibo.android.lab.newsapp.models.Article
 
 class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
@@ -14,12 +19,28 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
     lateinit var articleTitle: TextView
     lateinit var articleDescription: TextView
     lateinit var articleDateTime: TextView
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>(){
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+    val differ = AsyncListDiffer(this, differCallback)
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        TODO("Not yet implemented")
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+
     }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
