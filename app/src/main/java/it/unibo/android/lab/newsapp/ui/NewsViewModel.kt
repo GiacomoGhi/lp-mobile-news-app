a8725439523dd3daf6ba6f11e81d97832b5e4d29
tomm1341 <tommaso.foca@studio.unibo.it>
2024-05-29 18:59:06 +0200

    Add parameters

--- app/src/main/java/it/unibo/android/lab/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/ui/NewsViewModel.kt
@@ -1,10 +1,13 @@
 package it.unibo.android.lab.newsapp.ui
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import it.unibo.android.lab.newsapp.util.Resource
 import it.unibo.android.lab.newsapp.models.NewsResponse
+import it.unibo.android.lab.newsapp.repository.NewsRepository
 
-class NewsViewModel {
+class NewsViewModel (app: Application, val newsRepository: NewsRepository): AndroidViewModel(app){
 
     val headLines: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var headlinesPage = 1
