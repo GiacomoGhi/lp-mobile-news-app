11bbfe0d4ebe0c4dbe23d9f544a39c5f4c2cd1bb
tomm1341 <tommaso.foca@studio.unibo.it>
2024-06-07 10:54:39 +0200

    Completed new class

--- app/src/main/java/it/unibo/android/lab/newsapp/ui/NewsViewModelProviderFactory.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/ui/NewsViewModelProviderFactory.kt
@@ -1,11 +1,17 @@
 package it.unibo.android.lab.newsapp.ui
 
 import android.app.Application
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import it.unibo.android.lab.newsapp.repository.NewsRepository
 
-// ViewmodelProvider.Factory is a class that instantiate and return view model
+/***********************************************************************************
+   ViewmodelProvider.Factory is a class that instantiate and return view model
+   This class is only used to create an instance of NewsViewModel
+************************************************************************************/
 
 class NewsViewModelProviderFactory(val app: Application, val newsRepository: NewsRepository): ViewModelProvider.Factory {
-
+    override fun <T: ViewModel> create(modelClass: Class<T>): T {
+        return NewsViewModel(app, newsRepository) as T // Ensuring that the written instance matches w/ generic type T
+    }
 }
