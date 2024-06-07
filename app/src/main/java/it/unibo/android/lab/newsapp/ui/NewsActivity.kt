83b3613a3476b26d9722e4eb1cb9dd8ab1e8b944
tomm1341 <tommaso.foca@studio.unibo.it>
2024-06-07 10:57:50 +0200

    Set up view model in NewsActivity

--- app/src/main/java/it/unibo/android/lab/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/ui/NewsActivity.kt
@@ -6,12 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import it.unibo.android.lab.newsapp.R
+import it.unibo.android.lab.newsapp.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
+
+  lateinit var newsViewModel: NewsViewModel
+  lateinit var binding: ActivityNewsBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    binding = ActivityNewsBinding.inflate(layoutInflater)
     enableEdgeToEdge()
-    setContentView(R.layout.activity_news)
+    setContentView(binding.root)
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
