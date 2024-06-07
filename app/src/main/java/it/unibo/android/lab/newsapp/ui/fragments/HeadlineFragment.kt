c5b6bd59080721ce256b4fda23162f9c08aa819a
tomm1341 <tommaso.foca@studio.unibo.it>
2024-06-07 12:27:18 +0200

    Declared variables in HeadlineFragment.kt

--- app/src/main/java/it/unibo/android/lab/newsapp/ui/fragments/HeadlineFragment.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/ui/fragments/HeadlineFragment.kt
@@ -5,8 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import it.unibo.android.lab.newsapp.R
+import it.unibo.android.lab.newsapp.adapters.NewsAdapter
+import it.unibo.android.lab.newsapp.ui.NewsViewModel
 
 class HeadlineFragment : Fragment() {
+
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var retryButton: Button
+    lateinit var errorText: TextView
+    lateinit var itemHeadlinesError: CardView
+
 
 }
