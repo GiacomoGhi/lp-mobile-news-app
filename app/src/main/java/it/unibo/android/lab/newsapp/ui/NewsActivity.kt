a545496d12ae6c660bbe29557c46886fc0ee95d8
tomm1341 <tommaso.foca@studio.unibo.it>
2024-06-07 11:18:31 +0200

    Implemented navigation control

--- app/src/main/java/it/unibo/android/lab/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/ui/NewsActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import it.unibo.android.lab.newsapp.R
 import it.unibo.android.lab.newsapp.databinding.ActivityNewsBinding
 import it.unibo.android.lab.newsapp.db.ArticleDatabase
@@ -31,5 +33,9 @@ class NewsActivity : AppCompatActivity() {
     val newsRepository = NewsRepository(ArticleDatabase(this))
     val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
     newsViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+    val navController = navHostFragment.navController
+    binding.bottomNavigationView.setupWithNavController(navController)
   }
 }
