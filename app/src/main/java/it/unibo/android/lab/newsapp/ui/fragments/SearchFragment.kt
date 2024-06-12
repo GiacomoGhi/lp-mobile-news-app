70372529afd843dc3bd4017964e96c4b7d9e1d69
tomm1341 <tommaso.foca@studio.unibo.it>
2024-06-12 21:22:46 +0200

    Declared variables in SearchFragment.kt

--- app/src/main/java/it/unibo/android/lab/newsapp/ui/fragments/SearchFragment.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/ui/fragments/SearchFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.navigation.fragment.navArgs
 import it.unibo.android.lab.newsapp.R
+import it.unibo.android.lab.newsapp.adapters.NewsAdapter
 import it.unibo.android.lab.newsapp.databinding.FragmentArticleBinding
 import it.unibo.android.lab.newsapp.ui.NewsActivity
 import it.unibo.android.lab.newsapp.ui.NewsViewModel
 
 class SearchFragment  {
 
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var retryButton: Button
+    lateinit var errorText: TextView
+    lateinit var itemSearchError: CardView
 
 }
