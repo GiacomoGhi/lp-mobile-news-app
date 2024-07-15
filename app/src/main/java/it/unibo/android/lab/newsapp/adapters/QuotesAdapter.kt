f28c8b865c6185175ed298a063cd8777018f414e
tomm1341 <tommaso.foca@studio.unibo.it>
2024-07-15 17:15:55 +0200

    Completed QuoteViewHolder in QuotesAdapter

--- app/src/main/java/it/unibo/android/lab/newsapp/adapters/QuotesAdapter.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/adapters/QuotesAdapter.kt
@@ -1,12 +1,32 @@
 package it.unibo.android.lab.newsapp.adapters
 
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import it.unibo.android.lab.newsapp.R
 
 class QuotesAdapter : RecyclerView.Adapter<QuotesAdapter.QuotesViewHolder>() {
 
     inner class QuotesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val currencyPair: TextView = itemView.findViewById(R.id.currency_pair)
+        val percentageChange : TextView = itemView.findViewById(R.id.percentage_change)
+        val bidPrice : TextView = itemView.findViewById(R.id.bid_price)
+        val askPrice : TextView = itemView.findViewById(R.id.ask_price)
+        val lowPrice : TextView = itemView.findViewById(R.id.low_price)
+        val highPrice : TextView = itemView.findViewById(R.id.high_price)
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuotesViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: QuotesViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
 
 
