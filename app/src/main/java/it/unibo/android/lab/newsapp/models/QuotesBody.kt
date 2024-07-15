f20047b4975549adfd97f3421b04ade5741619aa
tomm1341 <tommaso.foca@studio.unibo.it>
2024-07-15 16:25:29 +0200

    Initialized quotesView with the error item

--- app/src/main/java/it/unibo/android/lab/newsapp/models/QuotesBody.kt
+++ app/src/main/java/it/unibo/android/lab/newsapp/models/QuotesBody.kt
@@ -3,11 +3,11 @@ package it.unibo.android.lab.newsapp.models
 import java.io.Serializable
 
 data class QuotesBody(
-    val ask: Double,
-    val bid: Double,
-    val currency: String,
-    val displayName: String,
-    val longName: String,
-    val symbol: String,
-    val regularMarketChangePercent: Double
+    val ask: Double, // prezzo a cui si vende (prezzo a sinistra)
+    val bid: Double, // prezzo a destra
+    val currency: String, // Valuta attuale (non nello screen, al momento omissibile, magari metterla di fianco all'ask) (quasi sempre USD)
+    val displayName: String, // Apple
+    val longName: String, // Apple inc ecc..
+    val symbol: String, // APL
+    val regularMarketChangePercent: Double //percentuale che sta sopra al nome (di variazione del prezzo in quel giorno)
 ): Serializable
